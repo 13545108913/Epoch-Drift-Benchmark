682d60b1aece391334da91d971032e5643af458b
ChenBoyu <3207218036@qq.com>
2025-11-14 21:41:37 +0800

    add ASI

--- Agents/asi/debug_actions/run_tests.sh
+++ Agents/asi/debug_actions/run_tests.sh
@@ -1,3 +1,3 @@
-# Run test for task 108
-python run_demo.py --websites admin --headless --task_name webarena.108 --action_path debug_actions/test_0.txt --rename_to webarena.108_test
+# Run test for task 419
+python run_demo.py --websites gitlab --headless --task_name myBenchmark.419 --action_path debug_actions/test_0.txt --rename_to myBenchmark.419_test
 
